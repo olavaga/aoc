78638bb7564dd999d63ca71c9dcc62b7e52d5e4e
Olav Nicolay Larsson Aga <olavnlaga@gmail.com>
2021-12-04 14:44:29 +0100

    :recycle: Small refactor

--- day3.py
+++ day3.py
@@ -1,39 +1,29 @@
-
 with open('day3.txt','r') as fil:
-    data = fil.readlines()
+    data = fil.read().splitlines()
 
-sums = [0]*12
-total = 0
-first_bits = []
-for line in data:
-    sums = [int(ch) + init for ch, init in zip(line, sums)]
-    total += 1
-
-most_common = "".join(str(round(sums[i] / total)) for i in range(12))
-least_common = "".join(str(round(1 - sums[i] / total)) for i in range(12))
-
-matches = data
-index = 0
-while 1 < len(matches):
-    ones = sum(int(line[index]) for line in matches)
-    zeros = sum(1 - int(line[index]) for line in matches)
-    bit = '1' if ones >= zeros else '0'
-    matches = [line for line in matches if line[index] == bit]
-    index += 1
-
-oxygen_rating = int(matches[0],2)
-
-matches = data
-index = 0
-while 1 < len(matches):
-    ones = sum(int(line[index]) for line in matches)
-    zeros = sum(1 - int(line[index]) for line in matches)
-    bit = '1' if ones < zeros else '0'
-    matches = [line for line in matches if line[index] == bit]
-    index += 1
-
-co2_scrubber = int(matches[0], 2)
+# Part one
+most_common = ""
+for i in range(len(data[0])):
+    ones = sum(int(line[i]) for line in data)
+    most_common += str(round(ones / len(data)))
+least_common = "".join('1' if bit == '0' else '0' for bit in most_common)
 
 print("Power consumption", int(most_common,2) * int(least_common,2))
+
+# Part two
+# Filter report data based on the bit criteria
+def searchReport(bitCriteria):
+    matches = data
+
+    for i in range(len(data[0])):
+        ones = sum(int(line[i]) for line in matches)
+        bit = bitCriteria(ones, len(matches) - ones)
+        matches = [line for line in matches if line[i] == bit]
+
+        if len(matches) == 1:
+            return int(matches[0],2)
+
+oxygen_rating = searchReport(lambda ones, zeros: '1' if ones >= zeros else '0')
+co2_scrubber = searchReport(lambda ones, zeros: '1' if ones < zeros else '0')
 
 print("Life support rating", oxygen_rating * co2_scrubber)
