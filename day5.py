f11769669f984fd8f41b0dabe9d9cc9681b3d697
Olav Nicolay Larsson Aga <olavnlaga@gmail.com>
2021-12-05 16:21:56 +0100

    :recycle: matrix -> sets

--- day5.py
+++ day5.py
@@ -10,20 +10,25 @@ with open('day5.txt', 'r') as fil:
         
         lines.append((a,b))
 
-# Create and plot chart
-chart = [[0] * 1000 for _ in range(1000)]
+# Keep track of all seen points, repeats are added to crosses
+seen = set()
+crosses = set()
 
 for a,b in lines:
+    # Account for stepping towards smaller numbers
     dirx = 1 if b[0] > a[0] else -1
     diry = 1 if b[1] > a[1] else -1
 
     xs = range(a[0], b[0] + dirx, dirx)
     ys = range(a[1], b[1] + diry, diry)
 
+    # Repeat single value when start is equal to end (horisontal/vertical)
     default = a[0] if a[0] == b[0] else a[1]
 
-    for x,y in zip_longest(xs, ys, fillvalue=default):
-        chart[x][y] += 1
+    points = set(zip_longest(xs, ys, fillvalue=default))
 
-# Count intersections and output
-print(sum(n > 1 for line in chart for n in line))
+    crosses |= seen & points
+    seen |= points
+
+# Output the count
+print(len(crosses))
