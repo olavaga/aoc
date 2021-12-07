3046950907bce37c0da636823ce5e8fb93b22aad
Olav Nicolay Larsson Aga <olavnlaga@gmail.com>
2021-12-07 10:09:20 +0100

    :zap: Added statistics library functions and used math to improve runtime

--- day7.py
+++ day7.py
@@ -1,12 +1,12 @@
 from math import floor
+import statistics
 
 with open('day7.txt', 'r') as fil:
     crabs = list(map(int,fil.readline().split(',')))
 
-crabs.sort()
-mean = crabs[len(crabs)//2]
-print("P1", sum(abs(it - mean) for it in crabs))
+middle_crab = statistics.median(crabs)
+print("P1", sum(abs(it - middle_crab) for it in crabs))
 
-average = floor(sum(crabs) / len(crabs))
-fuel_consumption = lambda n: sum(range(n+1))
-print("P2", sum(fuel_consumption(abs(it - average)) for it in crabs))
+average = floor(statistics.mean(crabs))
+fuel = lambda n: n*(n + 1)/2
+print("P2", sum(fuel(abs(it - average)) for it in crabs))
