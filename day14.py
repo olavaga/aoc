89f67702ccf8e2ffd9b91c28a1eab5710ff8574e
Olav Nicolay Larsson Aga <olavnlaga@gmail.com>
2021-12-14 10:03:55 +0100

    :art: Improve the structure of the code with functions

--- day14.py
+++ day14.py
@@ -1,39 +1,40 @@
 from collections import defaultdict, Counter
 from itertools import pairwise
 
-with open('data/day14.txt', 'r') as fil:
-    data = fil.read()
+def readLines(filnavn):
+    with open(filnavn, 'r') as fil:
+        data = fil.read().splitlines()
+    return [*map(str.strip, data)]
 
-template= None
-rules = defaultdict(str)
-for line in data.splitlines():
-    line = line.strip()
+def parseDict(data, sep):
+    return dict(line.split(sep) for line in data if sep in line)
 
-    if template is None:
-        lastChar = line[-1]
-        template = Counter(map("".join,pairwise(line)))
+def pairInsertion(pairs, rules):
+    newPairs = defaultdict(int)
 
-    elif line:
-        ab, c = line.split(' -> ')
-        rules[ab] = c
-
-
-def pairInsertion(template):
-    newTemplate = defaultdict(int)
-
-    for ab, count in template.items():
+    for ab, count in pairs.items():
         c = rules[ab]
-        newTemplate[ab[0] + c] += count
-        newTemplate[c + ab[1]] += count
+        newPairs[ab[0] + c] += count
+        newPairs[c + ab[1]] += count
 
-    return newTemplate
+    return newPairs
 
+def compute(steps, pairs, rules, lastChar):
+    for i in range(steps):
+        pairs = pairInsertion(pairs, rules)
 
-for i in range(40):
-    template = pairInsertion(template)
+    counts = {a:sum(v for k, v in pairs.items() if k[0]==a) for a in set("".join(pairs.keys()))}
+    counts[lastChar] += 1
 
-counts = {a:sum(v for k, v in template.items() if k[0]==a) for a in set("".join(template.keys()))}
-counts[lastChar] += 1
+    return max(counts.values()) - min(counts.values())
 
-result = max(counts.values()) - min(counts.values())
-print(f"{result=}")
+if __name__ == '__main__':
+    data = readLines('data/day14.txt')
+    template = data[0]
+    rules = parseDict(data, sep=' -> ')
+
+    pairs = Counter(map("".join,pairwise(template)))
+    lastChar = data[0][-1]
+
+    print(f"P1: {compute(10, pairs, rules, lastChar) = }")
+    print(f"P2: {compute(40, pairs, rules, lastChar) = }")
